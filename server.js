4cacc2349dbc89ae8f1451b9bd1d77cacefc3d3d
xwunderlich <xanderwunderlich@gmail.com>
2016-11-19 18:10:21 -0500

    basic server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var LocalStrategy = require('passport-local');
 var app = express();
 const PORT = 6969;
 app.use(passport.initialize());
+app.use(express.static(__dirname + '/src/html'));
 
 console.log("Spinning up server on port " + PORT)
 app.listen(PORT);
